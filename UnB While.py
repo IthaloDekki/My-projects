64d6f216087513a29798f2066ad87107ee6fe905
IthaloDekki <119539865+IthaloDekki@users.noreply.github.com>
2022-12-13 16:18:38 -0300

    Update UnB While.py

--- UnB While.py
+++ UnB While.py
@@ -1,3 +1,4 @@
+# UnB while 
 count = 0
 media = 0
 while True:
@@ -11,4 +12,22 @@ if count == 0 :
     print('0.00')
 else:
     print(f'{(media/count):.2f}')
+
+    
+    
+ # UnB Maior salario(While)
+
+maior_valor = 0
+
+while True:
+    nome, num = input().split(',')
+    if nome == "Fim":
+        break
+    num = float(num)
+    if num > maior_valor:
+        maior_valor = num 
+if maior_valor == 0:
+    print('Não tem')
+else:
+    print(f'{maior_valor:.2f}')
 
