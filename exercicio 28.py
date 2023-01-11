f8a52155a5be8161805e59becb596385e73fe747
Ithalo Junio Medeiros de O. Nóbrega <119539865+IthaloDekki@users.noreply.github.com>
2023-01-11 18:27:00 -0300

    Add files via upload

---
+++ exercicio 28.py
@@ -0,0 +1,14 @@
+from random import randint
+# função random retorna um número decimal aleatório 
+# randint retorna um número inteiro de 'inicio' até 'fim' ex.: randint(1,10)
+computador = randint(0, 5) # Faz o computador "Pensar"
+print('-=-' * 20 )
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar!')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei ?'))
+
+if jogador == computador :
+    print('Parabéns! Você conseguiu me vencer!')
+else:
+    print(f'Ganhei! Eu pensei no número {computador} e não no {jogador}')
+    
