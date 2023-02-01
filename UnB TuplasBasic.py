6313645fc74477ffcfe50905d0609e3700a05d1b
Ithalo Junio Medeiros de O. Nóbrega <119539865+IthaloDekki@users.noreply.github.com>
2023-01-31 22:02:31 -0300

    Update UnB TuplasBasic.py

--- UnB TuplasBasic.py
+++ UnB TuplasBasic.py
@@ -8,3 +8,9 @@ def stockmarket(stock):
             dic[stock[i][0]] += float[stock[i][1]] * int[stock[i][2]]
             
     return dic
+
+stock = [('24-Out-2020', 43.50, 25, 'CAT'),
+('25-Out-2020', 42.80, 50, 'ITU'),
+('26-Out-2020', 42.10, 75, 'ITU'),
+('27-Out-2020', 37.58, 100, 'GM')]
+print(stockmarket(stock))
